da1e8316355106b4705bf46681b1207d3add783f
Andres Felipe Lince Pineda <alince@COPSERVIR.COM>
2020-07-17 20:43:33 -0500

    Fin sección 4

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import {RouterModule, Routes} from '@angular/router';
+import { SharedModule } from './../shared/shared.module';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [    
+    ProgressComponent,
+    Grafica1Component,
+    DashboardComponent,
+    PagesComponent,
+  ],
+  imports: [   
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [    
+    ProgressComponent,
+    Grafica1Component,
+    DashboardComponent,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
